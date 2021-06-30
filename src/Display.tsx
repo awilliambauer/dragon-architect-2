35e58a00dd1c00c258379953d5b0e88049fc483d
Teagan Johnson <johnsont4@carleton.edu>
2021-06-30 12:38:30 -0500

    Added 2 spheres

--- src/Display.tsx
+++ src/Display.tsx
@@ -31,14 +31,48 @@ function Box(props: BoxProps) {
     )
 }
 
+type SphereProps = {
+    position: number[],
+}
+
+function Sphere(props: SphereProps) {
+    // This reference will give us direct access to the mesh
+    const mesh = useRef<MeshProps>()
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    // Rotate mesh every frame, this is outside of React without overhead
+    useFrame(() => {
+        if (mesh.current) {
+            mesh.current!.rotation.x = mesh.current.rotation.y += 0.01
+        }
+    })
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? 1.5 : 1}
+            onClick={(e) => setActive(!active)}
+            onPointerOver={(e) => setHover(true)}
+            onPointerOut={(e) => setHover(false)}>
+            <sphereGeometry args={[1, 50, 50]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'purple'} />
+        </mesh>
+    )
+}
+
 export default function Display() {
     return (
         <Canvas>
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <pointLight position={[-10, -10, -10]} />
+
             <Box position={[-1.2, 0, 0]} />
             <Box position={[1.2, 0, 0]} />
+
+            <Sphere position={[-3.5, 0, 0]} />
+            <Sphere position={[3.5, 0, 0]} />
         </Canvas>
     )
 }
