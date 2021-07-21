c079a4307e157cdd908c36644261caf1be4fafa1
KatrinaLiqy <lik@carleton.edu>
2021-07-22 07:06:12 +0800

    reset/run program button done

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import WorldState from './WorldState';
 import run, { load_stdlib, IncrementalSimulator, SimulatorState } from './Simulator';
 import parse, { Program, SyntaxError } from './Parser';
 import PuzzleState from './PuzzleState';
-import {Run} from './RunButton'
+import { Run } from './RunButton'
+import _ from 'lodash';
 
 export type GameState = {
-  world: WorldState,
+  program: Program
+  world: WorldState
   puzzle?: PuzzleState
   simulator: IncrementalSimulator
-  reset: Boolean
+  reset: boolean
+  lastSavedWorld: WorldState | undefined
 }
 
 class App extends React.Component<{}, GameState> {
@@ -23,55 +26,87 @@ class App extends React.Component<{}, GameState> {
 
     // set up initial state, will get overwritten in componentDidMount
     let world = new WorldState();
-    this.state = {
-      reset: false,
-      world: world,
-      simulator: new IncrementalSimulator(world, parse(`
+    let defaultProgram = parse(`
 repeat 4 times
     repeat 2 times
-      Forward(2)
+      Forward(4)
+    PlaceCube(1)
     Right()
-`) as Program)
+`) as Program;
+    this.state = {
+      program: defaultProgram,
+      reset: false,
+      world: world,
+      simulator: new IncrementalSimulator(world, defaultProgram),
+      lastSavedWorld: undefined
     }
     this.state.simulator.set_running();
     this.state.world.mark_dirty();
+    
   }
 
   componentDidMount() {
     // load the puzzle specification from puzzles/test.json
     // and then use it to set the game state
     PuzzleState.make_from_file("puzzles/test.json").then(p => {
-      let sim = new IncrementalSimulator(p.start_world, parse(`
+      let defaultProgram = parse(`
 repeat 4 times
     repeat 2 times
       Forward(4)
     PlaceCube(1)
     Right()
-`) as Program);
+`) as Program;
+      let sim = new IncrementalSimulator(p.start_world, defaultProgram);
       sim.sim_state = SimulatorState.Running;
       this.setState({
+        program: defaultProgram,
         world: p.start_world,
         puzzle: p,
         simulator: sim,
-        reset: false
+        reset: false,
+        lastSavedWorld: undefined
 
       });
     });
   }
 
   run_program() {
-    const program = blocks_to_text();
-    const ast = parse(program);
-    if (ast instanceof SyntaxError) {
-      console.error(`Syntax Error: ${ast}`);
-    } else {
-      run(this.state.world, ast);
+    // const program = blocks_to_text();
+    // const ast = parse(program);
+    // if (ast instanceof SyntaxError) {
+    //   console.error(`Syntax Error: ${ast}`);
+    // } else {
+    //   run(this.state.world, ast);
+    // }
+
+    if (!this.state.reset) { // run program
+      this.setState({
+        lastSavedWorld: _.cloneDeep(this.state.world)
+      })
+      const program = blocks_to_text();
+      const ast = parse(program);
+      if (ast instanceof SyntaxError) {
+        console.error(`Syntax Error: ${ast}`);
+      } else {
+        run(this.state.world, ast);
+      }
     }
+    else { // reset 
+      this.setState({
+        world: this.state.lastSavedWorld!,
+        lastSavedWorld: undefined
+      }, () => { this.state.world.mark_dirty() })
+      
+    }
+
+    //switch the button 
+    this.setState({
+      reset: !this.state.reset
+    });
+
   }
 
   render() {
-    //console.log(this.state.reset); 'true'
-    
     return (
       <div className="App">
         {/* Navigation bar */}
@@ -79,8 +114,9 @@ repeat 4 times
             Blockly
             Control buttons  */}
         <button onClick={() => console.log(blocks_to_text())}>blocks_to_text</button>
-        {/* <button onClick={() => this.run_program()}>Run Program</button> */}
-        <Run reset = {false} onClick = {() => {this.run_program(); this.setState({reset: true})}}/>
+        
+        <Run reset= {this.state.reset} world={this.state.world} program={this.state.program}
+         simulator={this.state.simulator} onClick = {() => {this.run_program()}} />
         
 
         {/* <button onClick={() => this.change_state()} */}
